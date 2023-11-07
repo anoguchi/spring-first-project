063a1b5377e307fa03cc8c61d983c43535de7bf2
Alberto Noguchi <alberto2611@gmail.com>
2023-11-06 23:56:25 -0300

    first commit

---
+++ src/test/java/com/firstproject/myfirstproject/MyFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstproject.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
